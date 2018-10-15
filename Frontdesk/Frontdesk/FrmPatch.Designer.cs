70781fe51d673184987870c3a52626b82a0cecc6
Ara Numan <nduo3nhylq@gmail.com>
2018-10-16 00:18:18 +0800

    In the beginning Ara created the Frm and the pnl. Now the projects was formless and empty, whiteness was over the surface of the Frm, and the mouse pointer was hovering over the empty Frm and pnl.

---
+++ Frontdesk/Frontdesk/FrmPatch.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Frontdesk
+{
+    partial class FrmPatch
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlPatch = new System.Windows.Forms.Panel();
+            this.pnlFooter = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pnlPatch.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlPatch
+            // 
+            this.pnlPatch.Controls.Add(this.button1);
+            this.pnlPatch.Location = new System.Drawing.Point(3, 3);
+            this.pnlPatch.Name = "pnlPatch";
+            this.pnlPatch.Size = new System.Drawing.Size(775, 531);
+            this.pnlPatch.TabIndex = 0;
+            // 
+            // pnlFooter
+            // 
+            this.pnlFooter.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(54)))), ((int)(((byte)(54)))));
+            this.pnlFooter.Location = new System.Drawing.Point(9, 540);
+            this.pnlFooter.Name = "pnlFooter";
+            this.pnlFooter.Size = new System.Drawing.Size(829, 21);
+            this.pnlFooter.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(126)))), ((int)(((byte)(225)))));
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(592, 476);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(178, 43);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "START FRONTDESK APP";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // FrmPatch
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(831, 564);
+            this.Controls.Add(this.pnlFooter);
+            this.Controls.Add(this.pnlPatch);
+            this.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "FrmPatch";
+            this.Text = "EAMD Electronic Application and Multimedia Development";
+            this.pnlPatch.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlPatch;
+        private System.Windows.Forms.Panel pnlFooter;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
